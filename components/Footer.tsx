8c1c7e28df381d851f62dc65bfa3eb8355f4781b
alnandoh <alnandoharsono@gmail.com>
2024-08-13 19:37:31 +0700

    Splitting map code and use dynamic import

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,11 @@ import { Mail, MapPin, Phone } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import FooterLogo from "@/public/NusaBean Logo - Normal.svg";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("./Map"), {
+  ssr: false,
+});
 
 export default function Footer() {
   return (
@@ -51,14 +56,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="relative w-full h-60 md:col-span-2 xl:h-80 rounded-xl overflow-hidden">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31682.3820074271!2d110.39271370742769!3d-6.97415470777513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e70f4b6d66b2df5%3A0xdabdca791c757a8c!2sJl.%20Brotojoyo%20No.4D%2022%2C%20RW.23%2C%20Plombokan%2C%20Kec.%20Semarang%20Utara%2C%20Kota%20Semarang%2C%20Jawa%20Tengah%2050171!5e0!3m2!1sen!2sid!4v1722789082326!5m2!1sen!2sid"
-            className="w-full h-full"
-            title="Nusa Bean Coffee Google Map Location"
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
+          <Map />
         </div>
       </div>
       <div className="bg-one-700 py-4">
