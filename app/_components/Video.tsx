6cd754531869f2705fb1453cc05243928f3dae9c
alnandoh <alnandoharsono@gmail.com>
2024-08-22 02:55:38 +0700

    style: layout change

--- app/_components/Video.tsx
+++ app/_components/Video.tsx
@@ -2,7 +2,7 @@ export default function Video() {
   return (
     <section className="bg-two-500 text-one-800" id="about-us">
       <div className="wrapper py-12 md:py-16 lg:py-24 xl:py-28">
-        <h2 className="text-center mb-4 md:mb-6 lg:mb-8">
+        <h2 className="lg:max-w-3xl mx-auto font-bold text-center leading-tight animate-fade-in mb-4 md:mb-6 lg:mb-8">
           Company Profile Video
         </h2>
         <div className="aspect-video rounded-lg md:rounded-xl overflow-hidden">
